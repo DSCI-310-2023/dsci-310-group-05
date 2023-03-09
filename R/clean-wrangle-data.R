becd33211408e26bb8e9832e24b26c31b61879ee
celike1 <ece01celik@gmail.com>
2023-03-09 13:37:22 -0800

    not final added functions

--- R/clean-wrangle-data.R
+++ R/clean-wrangle-data.R
@@ -1,13 +1,23 @@
-drugs <- select(raw_data, Age, Gender, Nscore, Escore, Oscore, Ascore, Nicotine, Cannabis) %>% 
-  mutate(Cannabis = str_replace_all(Cannabis, c("CL0" = "no", "CL1" = "no", "CL2" = "no", "CL3" = "yes", "CL4" = "yes", "CL5" = "yes", "CL6" = "yes"))) %>%          
-  mutate(Cannabis = as_factor(Cannabis)) %>% 
-  mutate(Nicotine = str_replace_all(Nicotine, c("CL0" = "no", "CL1" = "no", "CL2" = "no", "CL3" = "yes", "CL4" = "yes", "CL5" = "yes", "CL6" = "yes")))
-
-head(drugs)
 
 
-cannabis_and_nicotine <- group_by(drug_data, Nicotine, Cannabis) %>% 
-  summarize(n=n())
-cannabis_and_nicotine$label <- c("Neither", "Cannabis only", "Nicotine only", "Both") #This column will act as labels in the bar graph
+mutate_raw_data<- function (raw_data) {
+  
+  drugs <- select(raw_data, Age, Gender, Nscore, Escore, Oscore, Ascore, Nicotine, Cannabis) %>% 
+    mutate(Cannabis = str_replace_all(Cannabis, c("CL0" = "no", "CL1" = "no", "CL2" = "no", "CL3" = "yes", "CL4" = "yes", "CL5" = "yes", "CL6" = "yes"))) %>%          
+    mutate(Cannabis = as_factor(Cannabis)) %>% 
+    mutate(Nicotine = str_replace_all(Nicotine, c("CL0" = "no", "CL1" = "no", "CL2" = "no", "CL3" = "yes", "CL4" = "yes", "CL5" = "yes", "CL6" = "yes")))
+  
+  return(head(drugs))
+  
+}
 
-cannabis_and_nicotine
+
+wrangle_training_data<- function(training_data,strata_variable, predictor){
+
+  cannabis_and_nicotine <- group_by(training_data, predictor, strata_variable) %>% 
+    summarize(n=n())
+  cannabis_and_nicotine$label <- c("Neither", "Cannabis only", "Nicotine only", "Both") #This column will act as labels in the bar graph
+  
+  return(cannabis_and_nicotine)
+
+}
