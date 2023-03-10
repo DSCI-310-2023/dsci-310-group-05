e130cc1760fde51e565627553c6b9e6fe3869841
Angela Machado <angelalopesmachado@gmail.com>
2023-03-10 11:19:02 -0800

    Created load data and train/test data script and respective testthat functions

--- R/load-data-script.R
+++ R/load-data-script.R
@@ -1,17 +1,15 @@
 #' initial_data function
 #' Purpose: read raw data and add column names
 #' @param url the URL in which the data set is housed
-#' @returns head of the loaded data set
-#' @example initial_data("https://archive.ics.uci.edu/ml/machine-learning-databases/00373/drug_consumption.data")
-
-initial_data <- function(url) {
-  raw_data <- read.csv(url)
+#' @param col_names names of the columns
+#' @returns writes the loaded data into the data folder
+#' @example initial_data("drug_consumption.csv", c("ID", "Age", "Gender", "Education", "Country", "Ethnicity", "Nscore", "Escore", "Oscore", "Ascore", "Cscore", "Impulsive", "SS", "Alcohol", "Amphet", "Amyl", "Benzos", "Caff", "Cannabis", "Choc", "Coke", "Crack", "Ecstasy", "Heroin", "Ketamine", "Legalh", "LSD", "Meth", "Mushrooms", "Nicotine", "Semer", "VSA"))
+ 
+initial_data <- function(data, col_names) {
+  raw_data <- read.csv(data)
   
-  colnames(raw_data) <- c("ID", "Age", "Gender", "Education", "Country", "Ethnicity", "Nscore", "Escore", "Oscore", "Ascore", "Cscore", "Impulsive", "SS", "Alcohol", "Amphet",
-                          "Amyl", "Benzos", "Caff", "Cannabis", "Choc", "Coke", "Crack", "Ecstasy", "Heroin", "Ketamine", "Legalh", "LSD", "Meth", "Mushrooms", "Nicotine", "Semer",
-                          "VSA")
+  colnames(raw_data) <- col_names
   
-  return(head(raw_data))
+  write.csv(raw_data, "data/raw_data.csv")
 }
-
 
--- R/train-test-script.R
+++ R/train-test-script.R
@@ -1,10 +1,9 @@
-set.seed(1)
-
 #' split_dataset function
 #' Purpose: splits the data into testing/training and ensures variables are appropriate for classification algorithm
 #' @param data_set the data set to be used for the algorithm
 #' @param strata_variable a string representing the variable that will be set as the strata in initial_split
 #' @param predictor a string representing the variable to be used as the predictor for the algorithm
+#' @return writes training and testing data into the data folder
 #' @example split_dataset(drugs, Cannabis, Nicotine)
 
 split_dataset <- function(data_set, strata_variable, predictor) {
@@ -15,4 +14,7 @@ split_dataset <- function(data_set, strata_variable, predictor) {
   #setting predictor as numeric value
   testing_data <- mutate(testing_data, predictor = str_replace_all(predictor, c("no" = "0", "yes" = "1"))) %>% 
     mutate(predictor = as.numeric(predictor))
+  
+  write.csv(training_data, "data/training_data.csv")
+  write.csv(testing_data, "data/testing_data.csv")
 }
