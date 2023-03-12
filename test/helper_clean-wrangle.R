114f3d6b9dcc84b9d23a09cdcbcd0df369585993
celike1 <ece01celik@gmail.com>
2023-03-12 15:29:36 -0700

    added helper and test for wrangling data & minor changes to wrange function

---
+++ test/helper_clean-wrangle.R
@@ -0,0 +1,20 @@
+library(dplyr)
+
+# Define input data
+# Define test data
+training_data <- data.frame(predictor = c(rep("A", 4), rep("B", 4)),
+                            strata_variable = c(rep("X", 2), rep("Y", 2), rep("X", 2), rep("Y", 2)),
+                            value = 1:8)
+predictor <- "predictor"
+strata_variable <- "strata_variable"
+group_labels <- c("group1", "group2", "group3", "group4")
+
+# Define expected output
+expected_output <- data.frame(predictor = c("A", "A", "B", "B"),
+                              strata_variable = c("X", "Y", "X", "Y"),
+                              n = as.integer(c(2, 2, 2, 2)),
+                              label = group_labels)
+
+
+
+
