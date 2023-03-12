7f4088639719edf5a3d4545d249628902c3329e0
DaliaAh <ahmaddalia708@gmail.com>
2023-03-12 13:34:32 -0700

    made changes to test cases for predict-script.R

--- test/test_predict-script.R
+++ test/test_predict-script.R
@@ -1,32 +1,12 @@
 library(testthat)
-source("./../R/predict-script.R")
+source("./R/predict-script.R")
 
-data(mtcars)
-
-# Dummy test data
-test_data <- mtcars %>%
-  select(mpg, cyl, disp)
-
-# Dummy knn workflow object
-knn_wf <- knn(train = test_data, test = test_data$mpg, k = 3)
-
-# Test cases
-test_that("predict_drugs_workflow returns expected output", {
-  # Call the function
-  predict_drugs_workflow(knn_wf, test_data)
-  
-  # Check if the output file was created
-  expect_true(file.exists("data/prediction_data.csv"))
-  
-  # Check if the output file is not empty
-  expect_true(file.size("data/prediction_data.csv") > 0)
-  
-  # Check if the number of rows in the output matches the number of rows in the test data
-  expect_equal(nrow(read.csv("data/prediction_data.csv")), nrow(test_data))
-  
-  # Check if the output file has the expected column names
-  expect_equal(colnames(read.csv("data/prediction_data.csv")), c("pred", "mpg", "cyl", "disp"))
-  
-  # Check if the predicted values are within the range of actual values
-  expect_true(all(read.csv("data/prediction_data.csv")$pred >= min(test_data$mpg) & read.csv("data/prediction_data.csv")$pred <= max(test_data$mpg)))
+# Define the test cases for predict_drugs_workflow
+test_that("predict_drugs_workflow returns the expected output", {
+  #Test case 1
+  expect_identical(colnames(result), c(".pred_class", "X1", "X2", "X3"))
+  #Test case 2
+  expect_true(all(result$.pred_class %in% c("no", "yes")))
+  #Test case 3
+  expect_equal(result$.pred_class, mock_predictions$.pred_class)
 })
