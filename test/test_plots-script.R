2724f06c205ea57dc0f0b3274ed76f99b1104b10
Rithika Nair <122350530+Rithika-Nair@users.noreply.github.com>
2023-03-12 16:30:30 -0700

    minor fixes and added source()

--- test/test_plots-script.R
+++ test/test_plots-script.R
@@ -2,30 +2,31 @@
 #library(dplyr)
 library(testthat)
 source("./R/plots-script.R")
+source("./test/helper_plots.R")
 
 # Tests for the horizontal_hist function
 
 # Test case 1 
 test_that("horizontal_hist produces a plot", {
-  plot <- horizontal_hist(test_data, "x", "y", "x", "y", "Test Plot", plot_width = 6, plot_height = 4)
+  plot <- horizontal_hist(test_data_1, "x", "y", "x", "y", "Test Plot", plot_width = 6, plot_height = 4)
   expect_true(is.ggplot(plot))
 })
 
 # Test case 2 
 test_that("horizontal_hist produces the correct x-axis label", {
-  plot <- horizontal_hist(test_data, "x", "y", "x", "y", "Test Plot", plot_width = 6, plot_height = 4)
+  plot <- horizontal_hist(test_data_1, "x", "y", "x", "y", "Test Plot", plot_width = 6, plot_height = 4)
   expect_equal(xlab(plot)$label, xlab(expected_hist_plot)$label)
 })
 
 # Test case 3
 test_that("horizontal_hist produces the correct y-axis label", {
-  plot <- horizontal_hist(test_data, "x", "y", "x", "y", "Test Plot", plot_width = 6, plot_height = 4)
+  plot <- horizontal_hist(test_data_1, "x", "y", "x", "y", "Test Plot", plot_width = 6, plot_height = 4)
   expect_equal(ylab(plot)$label, ylab(expected_hist_plot)$label)
 })
 
 # Test case 4 
 test_that("horizontal_hist produces the correct title", {
-  plot <- horizontal_hist(test_data, "x", "y", "x", "y", "Test Plot", plot_width = 6, plot_height = 4)
+  plot <- horizontal_hist(test_data_1, "x", "y", "x", "y", "Test Plot", plot_width = 6, plot_height = 4)
   expect_equal(ggtitle(plot)$labels, ggtitle(expected_hist_plot)$labels)
 })
 
@@ -33,25 +34,25 @@ test_that("horizontal_hist produces the correct title", {
 # Tests for the scatterplot function
 # Test case 1 
 test_that("scatterplot produces a plot", {
-  plot <- scatterplot(test_data, "x", "y", "color", "x", "y", "Color", "Test Plot", plot_width = 6, plot_height = 4)
+  plot <- scatterplot(test_data_1, "x", "y", "color", "x", "y", "Color", "Test Plot", plot_width = 6, plot_height = 4)
   expect_true(is.ggplot(plot))
 })
 
 # Test case 2 
 test_that("scatterplot produces the correct x-axis label", {
-  plot <- scatterplot(test_data, "x", "y", "color", "x", "y", "Color", "Test Plot", plot_width = 6, plot_height = 4)
+  plot <- scatterplot(test_data_1, "x", "y", "color", "x", "y", "Color", "Test Plot", plot_width = 6, plot_height = 4)
   expect_equal(xlab(plot)$label, xlab(expected_scatter_plot)$label)
 })
 
 # Test case 3
 test_that("scatterplot produces the correct y-axis label", {
-  plot <- scatterplot(test_data, "x", "y", "color", "x", "y",  "Color", "Test Plot", plot_width = 6, plot_height = 4)
+  plot <- scatterplot(test_data_1, "x", "y", "color", "x", "y",  "Color", "Test Plot", plot_width = 6, plot_height = 4)
   expect_equal(ylab(plot)$label, ylab(expected_scatter_plot)$label)
 })
 
 # Test case 4 
 test_that("scatterplot produces the correct title", {
-  plot <- scatterplot(test_data, "x", "y", "color", "x", "y",  "Color", "Test Plot", plot_width = 6, plot_height = 4)
+  plot <- scatterplot(test_data_1, "x", "y", "color", "x", "y",  "Color", "Test Plot", plot_width = 6, plot_height = 4)
   expect_equal(ggtitle(plot)$labels, ggtitle(expected_scatter_plot)$labels)
 })
 
@@ -60,24 +61,24 @@ test_that("scatterplot produces the correct title", {
 
 # Test case 1 
 test_that("accuracy_plot produces a plot", {
-  plot <- accuracy_plot(input_data, x_label = "Neighbors", y_label = "Mean", plot_title = "Accuracy Plot")
+  plot <- accuracy_plot(test_data_2, x_label = "Neighbors", y_label = "Mean", plot_title = "Accuracy Plot")
   expect_true(is.ggplot(plot))
 })
 
 # Test case 2 
 test_that("accuracy_plot produces the correct x-axis label", {
-  plot <- accuracy_plot(input_data, x_label = "Neighbors", y_label = "Mean", plot_title = "Accuracy Plot")
+  plot <- accuracy_plot(test_data_2, x_label = "Neighbors", y_label = "Mean", plot_title = "Accuracy Plot")
   expect_equal(xlab(plot)$label, xlab(expected_accuracy_plot)$label)
 })
 
 # Test case 3
 test_that("accuracy_plot produces the correct y-axis label", {
-  plot <- accuracy_plot(input_data, x_label = "Neighbors", y_label = "Mean", plot_title = "Accuracy Plot")
+  plot <- accuracy_plot(test_data_2, x_label = "Neighbors", y_label = "Mean", plot_title = "Accuracy Plot")
   expect_equal(ylab(plot)$label, ylab(expected_accuracy_plot)$label)
 })
 
 # Test case 4 
 test_that("accuracy_plot produces the correct title", {
-  plot <- accuracy_plot(input_data, x_label = "Neighbors", y_label = "Mean", plot_title = "Accuracy Plot")
+  plot <- accuracy_plot(test_data_2, x_label = "Neighbors", y_label = "Mean", plot_title = "Accuracy Plot")
   expect_equal(ggtitle(plot)$labels, ggtitle(expected_accuracy_plot)$labels)
 })
