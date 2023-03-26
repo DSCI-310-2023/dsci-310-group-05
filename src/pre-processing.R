101871afb9bc489fa92b7d73944af1118656df20
Angela Machado <angelalopesmachado@gmail.com>
2023-03-25 21:01:45 -0700

    Finalised findings script

--- src/pre-processing.R
+++ src/pre-processing.R
@@ -13,7 +13,7 @@ Options:
     --dest_drugs_path=<dest_path>                 Path of where to save the drugs data
     --dest_test_path=<dest_path>                  Path of where to save the testing data
     --dest_train_path=<dest_path>                 Path of where to save the training data
-    --dest_processed_data=<dest_processed_data>   Path of where to save the fully processed data
+    --dest_processed_data=<dest_processed_data>   Path of where to save the fully processed training data
     "
 
 library(docopt)
@@ -30,19 +30,21 @@ strata <- opt$strata_variable
 predictor <- opt$predictor_variable
 
 
-drugs <- select(raw_data, Age, Gender, Nscore, Escore, Oscore, Ascore, Nicotine, Cannabis)
-drugs <- drugs %>% mutate(Cannabis = str_replace_all(Cannabis, c("CL0" = "no", "CL1" = "no", "CL2" = "no", "CL3" = "yes", "CL4" = "yes", "CL5" = "yes", "CL6" = "yes")))
-drugs <- drugs %>% mutate(Cannabis = as_factor(Cannabis))
-drugs <- drugs %>% mutate(Nicotine = str_replace_all(Nicotine, c("CL0" = "no", "CL1" = "no", "CL2" = "no", "CL3" = "yes", "CL4" = "yes", "CL5" = "yes", "CL6" = "yes")))
+drugs <- select(raw_data, Age, Gender, Nscore, Escore, Oscore, Ascore, Nicotine, Cannabis) %>% 
+  mutate(Cannabis = str_replace_all(Cannabis, c("CL0" = "no", "CL1" = "no", "CL2" = "no", "CL3" = "yes", "CL4" = "yes", "CL5" = "yes", "CL6" = "yes"))) %>%          
+  mutate(Cannabis = as_factor(Cannabis)) %>% 
+  mutate(Nicotine = str_replace_all(Nicotine, c("CL0" = "no", "CL1" = "no", "CL2" = "no", "CL3" = "yes", "CL4" = "yes", "CL5" = "yes", "CL6" = "yes")))
 
+set.seed(1) 
 train_test_data <- split_dataset(drugs, strata, predictor)
 
 training_data <- data.frame(train_test_data[1])
 testing_data <- data.frame(train_test_data[2])
 
-testing_data <- mutate(testing_data, Nicotine = str_replace_all(Nicotine, c("no" = "0", "yes" = "1"))) %>% mutate(Nicotine = as.numeric(Nicotine))
+testing_data <- mutate(testing_data, Nicotine = str_replace_all(Nicotine, c("no" = "0", "yes" = "1"))) %>% 
+  mutate(Nicotine = as.numeric(Nicotine))
 
-group_labels <- c("Neither", "Cannabis only", "Nicotine only", "Both") 
+group_labels <- c("Neither", "Cannabis only", "Nicotine only", "Both")
 
 cannabis_and_nicotine <- wrangle_training_data(training_data, Nicotine, Cannabis, group_labels)
 
