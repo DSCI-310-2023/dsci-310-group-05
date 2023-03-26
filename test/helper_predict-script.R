1a40f110231372486078bdc2c802d0b081501f7e
DaliaAh <ahmaddalia708@gmail.com>
2023-03-26 15:29:22 -0700

    added more tests for predict script and fixed documentation and helper scripts

--- test/helper_predict-script.R
+++ test/helper_predict-script.R
@@ -1,6 +1,6 @@
-# function inputs 
+# function inputs
 
-#Data
+# Data
 n <- 1000
 mock_pred_data <- data.frame(
   predicted_outcome = sample(c("no", "yes"), n, replace = TRUE),
@@ -9,34 +9,41 @@ mock_pred_data <- data.frame(
   X3 = rnorm(n)
 )
 
-#Test data
+# Test data
 test_data <- data.frame(
   X1 = c(0.5, 0.1, 0.7),
   X2 = c(0.9, 0.3, 0.6),
   X3 = c(0.2, 0.4, 0.8)
 )
 
-mock_spec <- nearest_neighbor(weight_func = "rectangular", neighbors = tune()) %>% 
-    set_engine("kknn") %>% 
-    set_mode("classification")
+mock_spec <- parsnip::nearest_neighbor(
+  weight_func = "rectangular",
+  neighbors = tune()
+) %>%
+  parsnip::set_engine("kknn") %>%
+  parsnip::set_mode("classification")
 
-mock_recipe <- recipe(as.formula(paste0("predicted_outcome", " ~ .")), data = mock_pred_data) %>% 
-    step_scale(all_predictors()) %>% 
-    step_center(all_predictors()) 
+mock_recipe <- recipes::recipe(as.formula(paste0("predicted_outcome", " ~ .")),
+  data = mock_pred_data
+) %>%
+  recipes::step_scale(all_predictors()) %>%
+  recipes::step_center(all_predictors())
 
-mock_knn_wf <- workflow() %>% 
-  add_recipe(mock_recipe) %>% 
-  add_model(mock_spec) %>% 
-  fit(data = mock_pred_data)
+mock_knn_wf <- workflows::workflow() %>%
+  workflows::add_recipe(mock_recipe) %>%
+  workflows::add_model(mock_spec) %>%
+  parsnip::fit(data = mock_pred_data)
 
 # Call the function with the test data
-result <- predict_drugs_workflow(mock_knn_wf, test_data)
+result <- pred_data <- stats::predict(mock_knn_wf, test_data) %>%
+  dplyr::bind_cols(test_data)
 
 # Define the expected outputs
-mock_predictions <- predict(mock_knn_wf, test_data)
+mock_predictions <- stats::predict(mock_knn_wf, test_data)
 
-#expected outputs
+# expected outputs
 expected_columns <- c(".pred_class", "X1", "X2", "X3")
 expected_results <- c("no", "yes")
 expected_predictions <- mock_predictions$.pred_class
-
+mock_pred_class <- "data.frame"
+not_dataframe <- list(a = 1, b = 2, c = 3)
