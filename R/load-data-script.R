34fa462721c5af57258042e9cebfb91870792e90
Angela Machado <angelalopesmachado@gmail.com>
2023-04-05 23:10:09 -0700

    Added exception handling cases for load data script

--- R/load-data-script.R
+++ R/load-data-script.R
@@ -14,7 +14,26 @@
 #' "Legalh", "LSD", "Meth", "Mushrooms", "Nicotine", "Semer", "VSA"))
 
 initial_data <- function(data, col_names) {
+
+  # Check that input file exists and is a CSV file
+  if (!file.exists(data)) {
+    stop("Error: Input data file does not exist.")
+  } else if (file_ext(data) != "csv") {
+    stop("Error: Input data file is not a CSV file.")
+  }
+
+  # Read in data
   raw_data <- utils::read.csv(data)
+
+  # Check that number of column names matches number of columns in input data
+  if (length(col_names) != ncol(raw_data)) {
+    stop(paste0(
+      "Error: Number of column names (", length(col_names),
+      ") does not match number of columns in input data (", ncol(raw_data), ")."
+    ))
+  }
+
+  # Set column names and return data frame
   colnames(raw_data) <- col_names
   return(raw_data)
 }
--- test/test_load-data.R
+++ test/test_load-data.R
@@ -18,3 +18,41 @@ test_that("initial_data function reads data and sets column names correctly", {
   # Test that the function returns a data frame
   expect_is(actual_data, "data.frame")
 })
+
+test_that("initial_data function raises error if input data file does
+          not exist", {
+  # Call the function with a non-existent file path
+  expect_error(initial_data("./data/nonexistent_file.csv", col_names))
+})
+
+test_that("initial_data function raises error if input data is not a CSV file", {
+  # Create a non-CSV file
+  file.create("./data/noncsv_file.txt")
+
+  # Call the function with the non-CSV file
+  expect_error(initial_data("./data/noncsv_file.txt", col_names))
+
+  # Delete the non-CSV file
+  file.remove("./data/noncsv_file.txt")
+})
+
+test_that("initial_data function raises error if input data file is empty", {
+  # Create an empty CSV file
+  write.csv(data.frame(), "./data/empty_file.csv")
+
+  # Call the function with the empty CSV file
+  expect_error(initial_data("./data/empty_file.csv", col_names))
+
+  # Delete the empty CSV file
+  file.remove("./data/empty_file.csv")
+})
+
+test_that("initial_data function raises error if number of column names does
+          not match number of columns in input data", {
+  # Create a column names vector with a different length than the number of
+  # columns in the input data
+  col_names_wrong <- c("col1", "col2")
+
+  # Call the function with the mismatched column names vector
+  expect_error(initial_data("./data/data_set.csv", col_names_wrong))
+})
